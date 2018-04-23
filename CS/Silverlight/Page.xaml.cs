5a6e15b45108ab7236787fd665c1c984cdd565c3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Silverlight/Page.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.AgDataGrid;
+using DevExpress.AgDataGrid.Internal;
+
+namespace DragAndDrop {
+	public partial class Page : UserControl, IDropableObject {		
+		public class DataItem {
+			public string Value {
+				get;
+				set;
+			}
+		}
+
+		DropController leftController;
+		DropController rightController;
+
+		public Page() {
+			InitializeComponent();
+			
+			//
+			// Create DragAndDrop Controllers Before Populating the grid
+			//
+
+			leftController = new DropController(this.leftGrid, this, this.DragSurface);
+			rightController = new DropController(this.rightGrid, this, this.DragSurface);
+				
+
+			//
+			// Populate Grids with Data
+			//
+
+			this.leftGrid.DataSource = new List<DataItem>() {
+				new DataItem() { Value = "Item 0" },
+				new DataItem() { Value = "Item 1" },
+				new DataItem() { Value = "Item 2" },
+				new DataItem() { Value = "Item 3" },
+				new DataItem() { Value = "Item 4" },
+			};
+			this.rightGrid.DataSource = new List<DataItem>() {
+				new DataItem() { Value = "Item 5" },
+				new DataItem() { Value = "Item 6" },
+				new DataItem() { Value = "Item 7" },
+				new DataItem() { Value = "Item 8" },
+				new DataItem() { Value = "Item 9" },
+			};
+		}		
+
+		#region IDropableObject Members
+
+		void IDropableObject.AcceptDrag(IDragableObject dragObject, Point position) {
+			DragObject obj = dragObject as DragObject;
+			if(obj == null) {
+				return;
+			}
+
+			// 
+			// Recycle
+			//
+			if(this.leftGrid == obj.Source || this.rightGrid == obj.Source) {
+				Point localPt = DropController.TransformSurface(position, this.recycler, this.DragSurface);
+				if(DropController.IsIn(this.recycler, localPt)) {
+					IList<DataItem> items = obj.Source.DataSource as IList<DataItem>;
+					items.Remove(obj.DataRow as DataItem);
+					obj.Source.Refresh();
+					return;
+				}
+			}
+
+			IList<DataItem> sourceItems = null;
+			IList<DataItem> destItems = null;
+
+			// 
+			// Right to Left
+			//
+			if(obj.Source == this.rightGrid) {
+				sourceItems = this.rightGrid.DataSource as IList<DataItem>;
+				destItems = this.leftGrid.DataSource as IList<DataItem>;
+			}
+
+			// 
+			// Left to Right
+			//
+			else if(obj.Source == this.leftGrid) {
+				sourceItems = this.leftGrid.DataSource as IList<DataItem>;
+				destItems = this.rightGrid.DataSource as IList<DataItem>;
+			}
+
+			DataItem dataItem = obj.DataRow as DataItem;
+			if(sourceItems != null && destItems != null && dataItem != null && sourceItems.Contains(dataItem) && !destItems.Contains(dataItem)) {
+				sourceItems.Remove(dataItem);
+				destItems.Add(dataItem);
+
+				this.leftGrid.Refresh();
+				this.rightGrid.Refresh();
+			}
+		}
+
+		bool IDropableObject.CanAccept(IDragableObject dragObject, Point position) {
+			DragObject obj = dragObject as DragObject;
+			if(obj == null) return false;
+
+			// 
+			// Recycle
+			//
+			if(this.leftGrid == obj.Source || this.rightGrid == obj.Source) {
+				
+				Point localPt = DropController.TransformSurface(position, this.recycler, this.DragSurface);
+				if(DropController.IsIn(this.recycler, localPt)) {
+					return true;
+				}
+
+
+			}
+
+			// 
+			// Left to Right
+			//
+			if(this.leftGrid == obj.Source) {
+				Point localPt = DropController.TransformSurface(position, this.rightGrid, this.DragSurface);
+				if(DropController.IsIn(this.rightGrid, localPt)) {
+					return true;
+				}
+			}
+			
+			// 
+			// Right to Left
+			//
+			else if(this.rightGrid == obj.Source) {
+				Point localPt = DropController.TransformSurface(position, this.leftGrid, this.DragSurface);
+				if(DropController.IsIn(this.leftGrid, localPt)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		bool IDropableObject.CanAccept() {
+			return true;
+		}
+
+		FrameworkElement IDropableObject.GetThumbObject() {
+			return null;
+		}
+
+		Rect IDropableObject.GetThumbRect(Point position) {
+			return new Rect();
+		}
+
+		#endregion
+	}
+}
---
+++ VB/Silverlight/Page.xaml.vb
@@ -0,0 +1,158 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports DevExpress.AgDataGrid
+Imports DevExpress.AgDataGrid.Internal
+
+Namespace DragAndDrop
+	Partial Public Class Page
+		Inherits UserControl
+		Implements IDropableObject
+		Public Class DataItem
+			Private privateValue As String
+			Public Property Value() As String
+				Get
+					Return privateValue
+				End Get
+				Set(ByVal value As String)
+					privateValue = value
+				End Set
+			End Property
+		End Class
+
+		Private leftController As DropController
+		Private rightController As DropController
+
+		Public Sub New()
+			InitializeComponent()
+
+			'
+			' Create DragAndDrop Controllers Before Populating the grid
+			'
+
+			leftController = New DropController(Me.leftGrid, Me, Me.DragSurface)
+			rightController = New DropController(Me.rightGrid, Me, Me.DragSurface)
+
+
+			'
+			' Populate Grids with Data
+			'
+
+			Me.leftGrid.DataSource = New List(Of DataItem) (New DataItem() {New DataItem() With {.Value = "Item 0"}, New DataItem() With {.Value = "Item 1"}, New DataItem() With {.Value = "Item 2"}, New DataItem() With {.Value = "Item 3"}, New DataItem() With {.Value = "Item 4"}})
+			Me.rightGrid.DataSource = New List(Of DataItem) (New DataItem() {New DataItem() With {.Value = "Item 5"}, New DataItem() With {.Value = "Item 6"}, New DataItem() With {.Value = "Item 7"}, New DataItem() With {.Value = "Item 8"}, New DataItem() With {.Value = "Item 9"}})
+		End Sub
+
+		#Region "IDropableObject Members"
+
+		Private Sub AcceptDrag(ByVal dragObject As IDragableObject, ByVal position As Point) Implements IDropableObject.AcceptDrag
+			Dim obj As DragObject = TryCast(dragObject, DragObject)
+			If obj Is Nothing Then
+				Return
+			End If
+
+			' 
+			' Recycle
+			'
+			If Me.leftGrid Is obj.Source OrElse Me.rightGrid Is obj.Source Then
+				Dim localPt As Point = DropController.TransformSurface(position, Me.recycler, Me.DragSurface)
+				If DropController.IsIn(Me.recycler, localPt) Then
+					Dim items As IList(Of DataItem) = TryCast(obj.Source.DataSource, IList(Of DataItem))
+					items.Remove(TryCast(obj.DataRow, DataItem))
+					obj.Source.Refresh()
+					Return
+				End If
+			End If
+
+			Dim sourceItems As IList(Of DataItem) = Nothing
+			Dim destItems As IList(Of DataItem) = Nothing
+
+			' 
+			' Right to Left
+			'
+			If obj.Source Is Me.rightGrid Then
+				sourceItems = TryCast(Me.rightGrid.DataSource, IList(Of DataItem))
+				destItems = TryCast(Me.leftGrid.DataSource, IList(Of DataItem))
+
+			' 
+			' Left to Right
+			'
+			ElseIf obj.Source Is Me.leftGrid Then
+				sourceItems = TryCast(Me.leftGrid.DataSource, IList(Of DataItem))
+				destItems = TryCast(Me.rightGrid.DataSource, IList(Of DataItem))
+			End If
+
+			Dim dataItem As DataItem = TryCast(obj.DataRow, DataItem)
+			If sourceItems IsNot Nothing AndAlso destItems IsNot Nothing AndAlso dataItem IsNot Nothing AndAlso sourceItems.Contains(dataItem) AndAlso (Not destItems.Contains(dataItem)) Then
+				sourceItems.Remove(dataItem)
+				destItems.Add(dataItem)
+
+				Me.leftGrid.Refresh()
+				Me.rightGrid.Refresh()
+			End If
+		End Sub
+
+		Private Function CanAccept(ByVal dragObject As IDragableObject, ByVal position As Point) As Boolean Implements IDropableObject.CanAccept
+			Dim obj As DragObject = TryCast(dragObject, DragObject)
+			If obj Is Nothing Then
+				Return False
+			End If
+
+			' 
+			' Recycle
+			'
+			If Me.leftGrid Is obj.Source OrElse Me.rightGrid Is obj.Source Then
+
+				Dim localPt As Point = DropController.TransformSurface(position, Me.recycler, Me.DragSurface)
+				If DropController.IsIn(Me.recycler, localPt) Then
+					Return True
+				End If
+
+
+			End If
+
+			' 
+			' Left to Right
+			'
+			If Me.leftGrid Is obj.Source Then
+				Dim localPt As Point = DropController.TransformSurface(position, Me.rightGrid, Me.DragSurface)
+				If DropController.IsIn(Me.rightGrid, localPt) Then
+					Return True
+				End If
+
+			' 
+			' Right to Left
+			'
+			ElseIf Me.rightGrid Is obj.Source Then
+				Dim localPt As Point = DropController.TransformSurface(position, Me.leftGrid, Me.DragSurface)
+				If DropController.IsIn(Me.leftGrid, localPt) Then
+					Return True
+				End If
+			End If
+
+			Return False
+		End Function
+
+		Private Function CanAccept() As Boolean Implements IDropableObject.CanAccept
+			Return True
+		End Function
+
+		Private Function GetThumbObject() As FrameworkElement Implements IDropableObject.GetThumbObject
+			Return Nothing
+		End Function
+
+		Private Function GetThumbRect(ByVal position As Point) As Rect Implements IDropableObject.GetThumbRect
+			Return New Rect()
+		End Function
+
+		#End Region
+	End Class
+End Namespace
